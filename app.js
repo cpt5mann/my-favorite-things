35790f84c829ed866a3ef349e741c62fad52e21d
Carter <chalfmann9547@myhta.org>
2018-02-15 14:22:49 -1000

    done

--- app.js
+++ app.js
@@ -3,23 +3,40 @@ var btn2= document.getElementById("button2");
 var btn3= document.getElementById("button3");
 var response1=document.getElementById("response1");
 
-//  CHAGGGGER
 
 var desserts=["Milkshakes", "Brownies", "Fruit"];
 var foods=["Tacos", "Burritos", "Musubis"];
-var games=["Destiny 2", "Destiny 1", "Madden"];
+var games=["Destiny 2", "Strat-o-Matic", "Madden"];
 
 btn1.addEventListener("click", function(){
 
     response1.innerHTML=("Look in the console!");
     
 var i;
-for(i=0;i>3;i=i+1){
+for(var i=0; i<desserts.length; i++){
     console.log(desserts[i]);
 }
 });
 
+btn2.addEventListener("click", function(){
 
+    response1.innerHTML=("Look in the console!");
+    
+var i;
+for(var i=0; i<foods.length; i++){
+    console.log(foods[i]);
+}
+});
+
+btn3.addEventListener("click", function(){
+
+    response1.innerHTML=("Look in the console!");
+    
+var i;
+for(var i=0; i<games.length; i++){
+    console.log(games[i]);
+}
+});
 
 
 
